d9c9b10b0a1f37732a5218e30b871c075d9e63e4
Hoang Sy Quy <kiaisoft.quy.hoang@gmail.com>
2023-12-22 16:04:47 +0700

    feat: header & modal

---
+++ src/components/SiteNavigation.vue
@@ -0,0 +1,28 @@
+<template>
+    <header class="sticky top-0 bg-weather-primary shadow-lg">
+        <nav class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6">
+            <RouterLink :to="{ name: 'home' }">
+                <div class="flex items-center gap-3 flex-1 text-2xl">
+                    <font-awesome-icon :icon="['fas', 'sun']" />
+                    <p class="text-2xl">The Weather App</p>
+                </div>
+            </RouterLink>
+
+            <div class="flex gap-3 flex-1 justify-end text-2xl">
+                <font-awesome-icon :icon="['fas', 'circle-info']" />
+                <font-awesome-icon :icon="['fas', 'plus']" />
+            </div>
+
+            <BaseModal>
+                <h2 class="text-black">Test</h2>
+            </BaseModal>
+        </nav>
+    </header>
+</template>
+
+<script setup>
+    import { RouterLink } from 'vue-router'
+    import BaseModal from './BaseModal.vue'
+</script>
+
+<style lang="scss" scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+// Icon imports
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faSun } from '@fortawesome/free-solid-svg-icons'
+import { faCircleInfo } from '@fortawesome/free-solid-svg-icons'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+library.add([faSun, faCircleInfo, faPlus])
+
+import './assets/tailwind.css'
+
+const app = createApp(App).component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(router)
 
